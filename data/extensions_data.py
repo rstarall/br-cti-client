15bb7259eb367da81b68d54ea51b29058b757cc0
Tuear <771227252@qq.com>
2024-11-20 18:16:47 +0800

    data_client update commit

---
+++ data/extensions_data.py
@@ -0,0 +1,61 @@
+import ipaddress
+import requests
+
+
+def ip_to_location(ip:str):
+    """
+    判断传入的ip地址是否为公网地址，是则返回对应的实际地理位置，否则返回标注“内网”
+    :param ip:IP地址
+    :return:实际地理位置或内网标注
+    """
+    # 验证ip地址是否有效
+    try:
+        ip_object = ipaddress.ip_address(ip)
+    except ValueError:
+        print(f"error：{ip} 不是一个有效的IP地址！")
+        return f"error：{ip} 不是一个有效的IP地址！"
+
+    # 私有地址
+    if ip_object.is_private:
+        print(f"{ip} 是私有地址")
+        return f"{ip} 是私有地址"
+
+    # 公网地址返回实际地理位置
+    try:
+        url = f"http://ip-api.com/json/{ip}"  # 构造调用ip-api服务的url，并设置返回数据为json格式（免费的查询服务只能用http）
+        response = requests.get(url, timeout=5)  # 发送请求，设置超时时间为5秒
+        response_data = response.json()
+        # 调用服务成功，返回地理信息
+        if response_data["status"] == "success":
+            print(f"{ip} 对应地理位置：{response_data['country']}, {response_data['regionName']}, {response_data['city']}")
+            return f"{ip} 对应地理位置：{response_data['country']}, {response_data['regionName']}, {response_data['city']}"
+
+        # 调用服务失败，打印报错信息
+        elif response_data["status"] == "fail":
+            print(f"{ip} 调用查询服务失败：{response_data['message']}")
+            return f"{ip} 调用查询服务失败：{response_data['message']}"
+
+    except Exception as e:
+        print(f"{ip} 查询地理位置时出错！error：{e}")
+        return f"{ip} 查询地理位置时出错！error：{e}"
+
+
+# 模块测试
+if __name__ == "__main__":
+    output = []  # 结果
+    # 测试用例
+    ip_test = [
+        "202.192.80.55",
+        "4564561.15362123",
+        "192.168.0.238",
+        "185.36.81.33",
+        "192.168.0.238",
+        "95.214.55.115",
+        "154.209.125.131",
+        "192.168.0.238",
+        "103.203.57.7"
+    ]
+    for ip in ip_test:
+        res = ip_to_location(ip)
+        output.append(res)
+    print(output)
